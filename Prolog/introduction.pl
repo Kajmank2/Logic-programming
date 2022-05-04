6965299cabe186324f1030c7b1ff5ee14aeb587b
Kajman <kaim1396@gmail.com>
2022-05-04 16:40:23 +0200

    Lists_prolog

--- Prolog/introduction.pl
+++ Prolog/introduction.pl
@@ -30,8 +30,40 @@ child(tommy, terry).
 child(jerry, john).
 child(jerry, alice).
 
+age(john, 73).
+age(alice,70).
+age(florence,38).
+age(terry,24).
+age(tommy,6).
+age(jerry,43).
+age(tomek,43).
+
 %RULES (; - alternative  ,- coniunction)
 parent(X,Y) :- child(Y,X).
 ancestor(X,Y) :- parent(X,Y); (parent(X,Z),ancestor(Z,Y)).
 descendand(X,Y) :- ancestor(Y,X).
-married(X,Y) :-  parent(X,Z), parent(Y,Z).
+married(X,Y) :-  parent(X,Z), parent(Y,Z), X \= Y.
+
+
+% =========AGE========
+older(X,Y) :- age(X,A), age(Y,B), A>B.
+younger(X,Y) :- age(X,A), age(Y,B), A<B.
+equeal(X,Y) :-age(X,A), age(Y,B), A==B.
+
+%=========LIKE========
+likes(tommy,apple).
+likes(tommy,potato).
+likes(terry,pear).
+likes(X,Y):- parent(Y,X); child(Y,X); married(Y,X).
+
+who_likes_to_eat_what :- food(Y),likes(X,Y), format('~w likes to eat ~w\n',[X,Y]),fail.
+/*
+ * Possible equeal
+ * X =:= Y -> EQUEAL
+ * X \= NOT EAQUEAL
+ * X =< Y
+ * X >= Y
+ *
+*/
+
+
